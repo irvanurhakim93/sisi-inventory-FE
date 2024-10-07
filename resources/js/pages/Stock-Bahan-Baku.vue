b980b6a920ee388524ba658815c1456df1158d6b
irvan_noerhakim23 <irvan.nurhakim1993@gmail.com>
2024-10-07 16:59:25 +0700

    Update Senin 7 Oktober 16:54

--- resources/js/pages/Stock-Bahan-Baku.vue
+++ resources/js/pages/Stock-Bahan-Baku.vue
@@ -1,3 +1,32 @@
 <template>
-    <img src="/assets/media/logos/excel-stock-bahan-baku.png" alt="">
+    <vue-office-excel
+        :src="excel"
+        style="height: 100vh;"
+        @rendered="renderedHandler"
+        @error="errorHandler"
+    />
 </template>
+
+<script>
+import VueOfficeExcel from '@vue-office/excel'
+import '@vue-office/excel/lib/index.css'
+
+export default {
+    components: {
+        VueOfficeExcel
+    },
+    data() {
+        return {
+            excel: '/assets/media/logos/task_inventory.xlsx'
+        }
+    },
+    methods: {
+        renderedHandler() {
+            console.log("Rendering Done")
+        },
+        errorHandler() {
+            console.log("Fail Rendering")
+        }
+    }
+}
+</script>
